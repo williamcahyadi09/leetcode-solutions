d9db0cbb9f3e098514f3f3bb41e4fde13ac1348a
William Cahyadi <43512375+williamcahyadi09@users.noreply.github.com>
2025-02-09 20:52:53 +0700

    Time: 81 ms (54.76%), Space: 39 MB (28.81%) - LeetHub

---
+++ 2364-count-number-of-bad-pairs/2364-count-number-of-bad-pairs.py
@@ -0,0 +1,14 @@
+class Solution:
+    def countBadPairs(self, nums: List[int]) -> int:
+        count = 0
+        len_nums = len(nums)
+        freq_dict = defaultdict(int)
+
+        total_pairs = len_nums * (len_nums - 1) // 2
+        good_pair_count = 0
+
+        for i in range(len_nums):
+            good_pair_count += freq_dict[nums[i]-i]
+            freq_dict[nums[i]-i] += 1
+
+        return total_pairs - good_pair_count
