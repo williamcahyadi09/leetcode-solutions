f9f31ec0596b316d3a5d17215f739174980c3847
William Cahyadi <43512375+williamcahyadi09@users.noreply.github.com>
2025-02-16 00:03:07 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1352-product-of-the-last-k-numbers/1352-product-of-the-last-k-numbers.py
@@ -0,0 +1,35 @@
+class ProductOfNumbers:
+
+    def __init__(self):
+        self.prefix_product = deque()
+        self.last_product = 1
+        self.idx = 0
+        
+
+    def add(self, num: int) -> None:
+        curr = self.last_product * num
+        self.prefix_product.append(curr)
+        if curr == 0:
+            self.last_product = 1
+            self.prefix_product = deque()
+            self.idx = 0
+        else:
+            self.last_product = curr
+            self.idx+=1
+
+
+    def getProduct(self, k: int) -> int:
+        print("self.prefix_product : ", self.prefix_product)
+        print("self.idx : ", self.idx)
+        if k > self.idx:
+            return 0
+        if k == self.idx:
+            return self.prefix_product[self.idx-1]
+        return self.prefix_product[self.idx-1] // self.prefix_product[self.idx-1-k]
+
+
+
+# Your ProductOfNumbers object will be instantiated and called as such:
+# obj = ProductOfNumbers()
+# obj.add(num)
+# param_2 = obj.getProduct(k)
